c045c71ade0a35c0d4a8f6ef9a961a25a22fffc5
Edmundo Jayr Perez Rosales <edmundo.jayr.perez.rosales@gmail.com>
2024-05-31 18:31:06 -0600

    Este fue el primer commit

--- src/main/java/com/jayr/curso/springboot/webapp/springbootweb/controllers/PathVariableController.java
+++ src/main/java/com/jayr/curso/springboot/webapp/springbootweb/controllers/PathVariableController.java
@@ -10,7 +10,9 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -41,6 +43,10 @@ public class PathVariableController {
 
     @Value("#{${config.valuesMap}}")
     private Map<String, Object> valuesMap; 
+
+
+    // @Autowired
+    //private Environment environment;
 
 
     @GetMapping("/vaz/{message}")
